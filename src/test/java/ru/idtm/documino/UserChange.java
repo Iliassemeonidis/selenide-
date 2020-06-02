7b68f8ae3e7c9b6d5db0e7fa6a2f219090264a6c
SemeonidisIV <i.semeonidis@id-mt.ru>
2020-06-02 22:40:39 +0300

    Обновил смок тест 02062020

--- src/test/java/ru/idtm/documino/UserChange.java
+++ src/test/java/ru/idtm/documino/UserChange.java
@@ -18,24 +18,42 @@ public class UserChange {
         $(byXpath("/html/body/div/div[3]/form/div[1]/input")).setValue("Boss11").pressEnter();
         $(byXpath("/html/body/div/div[3]/form/div[2]/input")).setValue("1");
         $(byText("Войти")).click();
+        sleep(1000);
     }
     public static void comInBoss51(){
         sleep(1000);
         $(byXpath("/html/body/div/div[3]/form/div[1]/input")).setValue("Boss51").pressEnter();
         $(byXpath("/html/body/div/div[3]/form/div[2]/input")).setValue("1");
         $(byText("Войти")).click();
+        sleep(1000);
     }
     public static void comInBoss31(){
         sleep(1000);
         $(byXpath("/html/body/div/div[3]/form/div[1]/input")).setValue("Boss31").pressEnter();
         $(byXpath("/html/body/div/div[3]/form/div[2]/input")).setValue("1");
         $(byText("Войти")).click();
+        sleep(1000);
+    }
+    public static void comInBoss32(){
+        sleep(1000);
+        $(byXpath("/html/body/div/div[3]/form/div[1]/input")).setValue("Boss32").pressEnter();
+        $(byXpath("/html/body/div/div[3]/form/div[2]/input")).setValue("1");
+        $(byText("Войти")).click();
+        sleep(1000);
+    }
+    public static void comInClerk2(){
+        sleep(1000);
+        $(byXpath("/html/body/div/div[3]/form/div[1]/input")).setValue("Clerk2").pressEnter();
+        $(byXpath("/html/body/div/div[3]/form/div[2]/input")).setValue("1");
+        $(byText("Войти")).click();
+        sleep(1000);
     }
     public static void comInBoss21(){
         sleep(1000);
         $(byXpath("/html/body/div/div[3]/form/div[1]/input")).setValue("Boss21").pressEnter();
         $(byXpath("/html/body/div/div[3]/form/div[2]/input")).setValue("1");
         $(byText("Войти")).click();
+        sleep(1000);
     }
 
 
--- src/test/java/ru/idtm/documino/WorkWithDocuments.java
+++ src/test/java/ru/idtm/documino/WorkWithDocuments.java
@@ -6,9 +6,34 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class WorkWithDocuments {
 
-    public static void tryToFind(){
+    public static void tryToFindDocWithDescription(String description){
         $(byXpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div/button")).click();
-        $(byXpath("//*[@id=\"desc\"]")).setValue("Auto-test777");
+        $(byXpath("//*[@id=\"desc\"]")).setValue(description);
+        sleep(10000);
+        $(byXpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div/div[4]/div[2]/button")).click();
+        sleep(1000);
+        $$(byText("Распорядительные документы"))
+                .first()
+                .click();
+
+    }
+    public static void tryToFindDocWithRegNumber(String regNumber){
+        $(byXpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div/button")).click();
+        $(byXpath("//*[@id=\"desc\"]")).setValue(regNumber);
+        sleep(10000);
+        $(byXpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div/div[4]/div[2]/button")).click();
+        sleep(1000);
+        $$(byText("Распорядительные документы"))
+                .first()
+                .click();
+
+    }
+
+
+    public static void tryToFindDocWithSinger(String singer) {
+        // ием по подписанту
+        $(byXpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div/button")).click();
+        $(byXpath("//*[@id=\"desc\"]")).setValue(singer);
         sleep(10000);
         $(byXpath("/html/body/div[1]/div[3]/div/div[2]/div[2]/div[1]/div[2]/div/div[1]/div/div[1]/div[1]/div[1]/div/div[4]/div[2]/button")).click();
         sleep(1000);
@@ -16,4 +41,7 @@ public class WorkWithDocuments {
                 .first()
                 .click();
     }
+
+
+
 }
